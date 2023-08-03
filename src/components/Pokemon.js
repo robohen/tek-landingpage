17569f73f09c0f8fec46062ceee9684e28bf6ba4
robohen <88636103+robohen@users.noreply.github.com>
2023-08-03 12:27:36 -0700

    pokemon api complete need to work on dark mode still

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,36 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Pokemon = () => {
+  const [randomPokemon, setRandomPokemon] = useState(null);
+
+  const handleRandomPokemon = () => {
+    // Generate a random number between 1 and 898 (total number of Pokémon in the API)
+    const randomId = Math.floor(Math.random() * 898) + 1;
+
+    // Make the API call to get the random Pokémon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the state with the fetched Pokémon data
+        setRandomPokemon(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokémon data:', error);
+      });
+  };
+
   return (
-    <div className='contact  night-mode-available text'>
+    <div className='contact night-mode-available text'>
       <h1>Pokemon</h1>
-      <h3>Find out what pokemon you recieve randomly!</h3>
-      <button>Random</button>
+      <h3>Find out what pokemon you receive randomly!</h3>
+      <button onClick={handleRandomPokemon}>Random</button>
+      {randomPokemon && (
+        <div>
+          <h1>{randomPokemon.name}</h1>
+          <img src={randomPokemon.sprites.front_default} alt={randomPokemon.name} style={{width: 300, height: 300}} />
+
+        </div>
+      )}
     </div>
   );
 };
