1d5cc4057dca4d517f63ed5ce261c81cfcdc5979
robohen <88636103+robohen@users.noreply.github.com>
2023-08-08 11:55:27 -0700

    updated linkedin url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentYear(new Date().getFullYear());
+    }, 1000 * 60 * 60 * 24); // Updates every day
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="footer night-mode-available text body">
       <a href="https://github.com/robohen" target="_blank" rel="noopener noreferrer">
@@ -10,8 +20,8 @@ const Footer = () => {
           alt="github-icon"
         />
       </a>
-      <span className="span">©2023 HenryCanCode</span>
-      <a href="https://www.linkedin.com/in/henry-gutierrez-27b65a247/" target="_blank" rel="noopener noreferrer">
+      <span className="span">&#169;{currentYear} HenryCanCode</span>
+      <a href="https://www.linkedin.com/in/henrygutierrez1995/" target="_blank" rel="noopener noreferrer">
         <img
           className="linkedinicon"
           src="https://img.icons8.com/ios-glyphs/452/linkedin.png"
